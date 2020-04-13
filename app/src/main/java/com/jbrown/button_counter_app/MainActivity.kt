de0b1cca43a32a23cf8a9a4a1abb12a6ea389ac6
captdread <jamesbrownre1@gmail.com>
2020-04-12 23:04:31 -0400

    Adding files

---
+++ app/src/main/java/com/jbrown/button_counter_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jbrown.button_counter_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
